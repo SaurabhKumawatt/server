7ce2ca75b6662ef2466d76216ff8f1149c735fe5
saurabhkumawatt <saurabhkumawat8959@gmail.com>
2025-06-04 19:55:50 +0530

    dev mode

--- controllers/userController.js
+++ controllers/userController.js
@@ -66,7 +66,7 @@ exports.registerUser = async (req, res) => {
     const token = generateToken(newUser._id);
     res.cookie("token", token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === "production",
+      secure: process.env.NODE_ENV === "development",
       sameSite: "None",
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });

5ba927ecb39485b5354ed6da84c9ac4293113fe9
saurabhkumawatt <saurabhkumawat8959@gmail.com>
2025-06-15 21:50:41 +0530

    active plewn

--- controllers/userController.js
+++ controllers/userController.js
@@ -214,8 +214,9 @@ exports.getLoggedInUserProfile = async (req, res) => {
   try {
     const user = await User.findById(req.user._id)
       .select("-password")
-      .populate("enrolledCourses.course", "title price");
-
+      .populate("enrolledCourses.course", "title price isBundle");
+    console.log(user);
+    
     if (!user) {
       return res.status(404).json({ message: "User not found" });
     }

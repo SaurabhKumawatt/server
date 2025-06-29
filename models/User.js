b8c55313d5be243d2399b971db9543af3cc4416c
saurabhkumawatt <saurabhkumawat8959@gmail.com>
2025-06-30 00:01:44 +0530

    username validation remove unique

--- models/User.js
+++ models/User.js
@@ -12,7 +12,6 @@ const userSchema = new mongoose.Schema(
     username: {
       type: String,
       required: [true, "Username is required"],
-      unique: true,
       trim: true,
       minlength: [3, "Username must be at least 3 characters"],
     },
